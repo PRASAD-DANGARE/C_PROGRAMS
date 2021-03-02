b52d967c7c20384342efb71c57cb9f8382244022
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-03-02 14:44:35 +0530

    Accept No, Display Even Factor Of That No
    Accept Number From User And Display Even Factors Of That Numbers

---
+++ Assignment3_2.c
@@ -0,0 +1,46 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Function Name :  DisplayFactor()
+// Description   :  Accept Number From User And Display Even Factors Of That Numbers
+// Input         :  Integer
+// Output        :  Integer
+// Author        :  Prasad Dangare
+// Date          :  02 Mar 2021
+//
+////////////////////////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+
+void DisplayFactor(int iNo)
+{
+    int i = 0;
+
+    if(iNo <= 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(i = 1; i < iNo; i++)
+    {
+        if (iNo % i == 0)
+        {
+            if(i % 2 == 0)
+            {
+                printf("%d ", i);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    
+    printf("Enter Number : ");
+    scanf("%d", &iValue);
+    
+    DisplayFactor(iValue);
+    
+    return 0;
+}
