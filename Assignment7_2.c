f19e235e9d3af3ae64de9b906393409b6ffa45f9
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-03-06 15:26:09 +0530

    Accept No From User & Return Count Of Odd Digits

---
+++ Assignment7_2.c
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Function Name :  CountOdd()
+// Description   :  Accept Number From User And Return Count Of Odd Digits
+// Input         :  Integer
+// Output        :  Integer
+// Author        :  Prasad Dangare
+// Date          :  06 Mar 2021
+//
+////////////////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+
+int CountOdd(int iNo)
+{
+    int iDigit = 0, iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo =- iNo;
+    }
+
+    while(iNo != 0)
+    {
+        if(((iNo % 10 ) % 2) != 0)
+        {
+            iCnt++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCnt;
+
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+ 
+    printf("Enter number : ");
+    scanf("%d", &iValue);
+ 
+    iRet = CountOdd(iValue);
+    printf("%d", iRet);
+ 
+    return 0;
+}
