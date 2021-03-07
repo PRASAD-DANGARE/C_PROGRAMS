b0da5c5b266ba475967901a64c3de3d179ad58ae
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-03-07 16:25:37 +0530

    Get Digit, Return Addition Of All Digits V2
    Accept Digit From User And Return Addition Of All Digits Of That Number V2

---
+++ Program21.c
@@ -0,0 +1,41 @@
+///////////////////////////////////////////////////////////
+//
+// Function Name :  CheckPrime()
+// Input         :  Integer
+// Output        :  Integer
+// Description   :  Accept Digit From User And Return Addition Of All Digits Of That Number V2
+// Author        :  Prasad Dangare
+// Date          :  06 Mar 2021
+//
+///////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+
+int SumDigits(int iNo)
+{
+    int iDigit = 0, iSum = 0;
+    if(iNo < 0)
+    {
+        iNo =- iNo;
+    }
+    while (iNo > 0)
+    {
+       iSum = iSum + (iNo % 10);
+       iNo = iNo / 10;
+    }
+    return iSum;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    printf("Enter Number : ");
+    scanf("%d", &iValue);
+
+    iRet = SumDigits(iValue);
+    printf("Sum is : %d", iRet);
+
+    return 0;
+}
