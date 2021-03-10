d0e1a7d234d6e305c5d03147bd44afb459da55cd
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-03-10 18:01:16 +0530

    Accept No & Return Largest Digit V2

---
+++ Program30.c
@@ -0,0 +1,52 @@
+///////////////////////////////////////////////////////////
+//
+// Function Name :  MaxDigit()
+// Input         :  Integer
+// Output        :  Integer
+// Description   :  Accept Number From User And Return Largest Digit, Reduce Time Complexcity V2
+// Author        :  Prasad Dangare
+// Date          :  10 Mar 2021
+//
+///////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int MaxDigit(int iNo)
+{
+    int iDigit = 0, iMax = 0;
+
+    if(iNo < 0)
+    {
+        iNo =- iNo;
+    }
+
+    while (iNo > 0)
+    {
+        iDigit = iNo % 10;
+        if(iDigit > iMax)
+        {
+            iMax = iDigit;
+            if(iMax == 9)
+            {
+                break;
+            }
+        }
+        iNo = iNo / 10;
+    }
+    return iMax;
+}
+
+int main()
+{
+    int iValue = 0;
+    int Ret = 0;
+
+    printf("Enter The Number : ");
+    scanf("%d", &iValue);
+    
+    Ret = MaxDigit(iValue);
+
+    printf("Largest Digit : %d ",Ret);
+
+    return 0;
+}
