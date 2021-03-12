d77817b80302cdbd1d785e2b753dbc6c0708ac62
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-03-12 17:38:40 +0530

    Display NumberIn Rectangle Pattern In ReverseOrder

---
+++ Assignment11_3.c
@@ -0,0 +1,49 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Function Name :  Pattern()
+// Description   :  Display Number In Rectangle Pattern In Reverse Order
+// Input         :  Integer
+// Output        :  Integer
+// Author        :  Prasad Dangare
+// Date          :  12 Mar 2021
+//
+////////////////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow <= 0)
+    {
+        printf("Invalid Number ");
+        return;
+    }
+
+    printf("\n");
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = iCol; j >= 1; j--)
+        {
+            printf("%d\t", j);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows : ");
+    scanf("%d", &iValue1);
+
+    printf("Enter number of cols : ");
+    scanf(" %d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
