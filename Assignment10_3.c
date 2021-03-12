82c253f414c7b2ba7ffbd8e376bcb4e7d9de95ca
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-03-12 17:35:44 +0530

    Display Pattern Input 5 - 1 * 2 * 3 * 4 * 5 *

---
+++ Assignment10_3.c
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Function Name :  Pattern()
+// Description   :  Display Pattern Input 5 - 1 * 2 * 3 * 4 * 5 *
+// Input         :  Integer
+// Output        :  Integer
+// Author        :  Prasad Dangare
+// Date          :  12 Mar 2021
+//
+////////////////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        printf("Enter Number Which Is Greater Than 0 ");
+        return;
+    }
+
+    printf("\n");
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("%d\t*\t", iCnt);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+    
+    printf("Enter number of elements : ");
+    scanf("%d", &iValue); 
+
+    Pattern(iValue);
+
+    return 0;
+}
