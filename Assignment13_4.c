f74e16bcd6061459836bc7cc99e4261ef2880b2e
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-03-16 18:10:43 +0530

    Accept Number From User & Display @ in Table

---
+++ Assignment13_4.c
@@ -0,0 +1,54 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Function Name :  Pattern()
+// Description   :  Accept Number From User & Display @ in Table  
+// Input         :  Integer
+// Output        :  Integer
+// Author        :  Prasad Dangare
+// Date          :  15 Mar 2021
+//
+////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void Pattern(unsigned int iRow, unsigned int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iCol)
+    {
+        return;
+    }
+
+    printf("\n");
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if((i == 1) || (j == 1) || (i == iCol) || (j == iRow)) 
+            {
+                printf("#\t");
+            }
+            else
+            {
+                printf("@\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    unsigned int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter Number of rows : ");
+    scanf("%u", &iValue1);
+
+    printf("Enter Number of cols : ");
+    scanf("%u", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
