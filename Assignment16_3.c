992cef58fec73663c50691448b2582515984f014
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-03-17 15:07:14 +0530

    Pattern Printing

---
+++ Assignment16_3.c
@@ -0,0 +1,50 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Function Name :  Pattern()  
+// Input         :  Integer
+// Output        :  Integer
+// Author        :  Prasad Dangare
+// Date          :  16 Mar 2021
+//
+////////////////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    printf("\n");
+    for(i = iRow; i >= 1; i--)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if((i == 1) || (j == 1) || (i == iCol) || (j == iRow)) 
+            {
+                printf("*\t");
+            }
+            else if (i == j)
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf(" \t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns : ");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
