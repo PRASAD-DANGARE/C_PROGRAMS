96638485622d0e2523a9b96c42da92189a216dea
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-03-17 15:18:42 +0530

    Accept Row & Column And Display Numbers, *, $

---
+++ Program56.c
@@ -0,0 +1,60 @@
+///////////////////////////////////////////////////////////
+//
+// Function Name :  Pattern()
+// Input         :  Integer
+// Output        :  Integer
+// Description   :  Accept Row & Column And Display Numbers, *, $
+// Author        :  Prasad Dangare
+// Date          :  15 Mar 2021
+//
+///////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+
+void Pattern(unsigned int iRow, unsigned int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iCol)
+    {
+        return;
+    }
+    printf("\n");
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i == j)
+            {
+                printf("%d\t", j);
+            }
+
+            else if ( i > j)
+            {
+                printf("*\t");
+            }
+            
+            else
+            {
+                printf("$\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    unsigned int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter Number of rows : ");
+    scanf("%u", &iValue1);
+
+    printf("Enter Number of cols : ");
+    scanf("%u", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
