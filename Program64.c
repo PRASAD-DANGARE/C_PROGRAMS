61e2683106347a0a816a8b69cd71466bd1533b80
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-03-18 11:16:07 +0530

    Accept Row & Column And Display Flag V3

---
+++ Program64.c
@@ -0,0 +1,60 @@
+///////////////////////////////////////////////////////////
+//
+// Function Name :  Pattern()
+// Input         :  Integer
+// Output        :  Integer
+// Description   :  Accept Row & Column And Display Flag V3
+// Author        :  Prasad Dangare
+// Date          :  16 Mar 2021
+//
+///////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+
+void Pattern(unsigned int iRow, unsigned int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iCol)
+    {
+        return;
+    }
+
+    printf("\n");
+    
+    for(i = iRow; i >= 1; i--)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("*\t");
+        }
+        printf("\n");
+    }
+    /*for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i <= j)
+            {
+                printf("#\t");
+            }
+        }
+        printf("\n");
+    }*/
+}
+
+int main()
+{
+    unsigned int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter Number of rows : ");
+    scanf("%u", &iValue1);
+
+    printf("Enter Number of cols : ");
+    scanf("%u", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
