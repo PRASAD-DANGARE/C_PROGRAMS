b45f5597c35544697874ecb3060e85dc670761f7
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-03-21 15:39:22 +0530

    Accept No & Display Even Ele & Divisible By 5

---
+++ Assignment17_3.c
@@ -0,0 +1,60 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Function Name :  Even()  
+// Description   :  Accept Number From User & Display Elements Which Are Even & Divisible By 5 
+// Input         :  Integer
+// Output        :  Integer
+// Author        :  Prasad Dangare
+// Date          :  19 Mar 2021
+//
+////////////////////////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void Even(int Arr[], int iSize)
+{
+    int i = 0;
+
+    if((Arr == NULL) || (iSize <= 0))
+    {
+        return;
+    }
+
+    printf("Elements Divisible By 5 And 2 Are : \n");
+    for(i = 0; i < iSize; i++)
+    {
+        if((Arr[i] % 2 == 0) && (Arr[i] % 5 == 0))
+        {
+            printf("%d\n", Arr[i]); 
+        }
+    }
+}
+int main()
+{
+    int *arr = NULL; // pointer
+    int iLength = 0, i = 0;
+
+    printf("Enter Number Of Elements : ");
+    scanf("%d", &iLength);
+
+    arr = (int*)malloc(iLength * sizeof(int));
+    if(arr == NULL)
+    {
+        printf("Unable To Allocate The Memory ");
+        return -1;
+    }
+
+    printf("Enter Elements : \n");
+    for(i = 0; i < iLength; i++)
+    {
+        scanf("%d", &arr[i]);
+    }   
+
+    Even(arr, iLength);
+
+    free(arr);
+
+    return 0; 
+}
