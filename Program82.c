3bded167896aec3dc70d7d38b1eb8affb8fb55dc
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-03-23 11:30:41 +0530

    Accept String & Count The Number Of Letters a

---
+++ Program82.c
@@ -0,0 +1,47 @@
+///////////////////////////////////////////////////////////
+//
+// Function Name :  CountA()  
+// Input         :  Integer
+// Output        :  Integer
+// Description   :  Accept String & Count The Number Of Letters a
+// Author        :  Prasad Dangare
+// Date          :  22 Mar 2021
+//
+///////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+
+int CountA(char str[])
+{
+    int i = 0;
+    
+    if(str == NULL)
+    {
+        return 0;
+    }
+
+    while (*str != '\0')
+    {
+        if(*str == 'a')
+        {
+            i++;
+        }    
+        str++;
+    }
+    return i;
+    
+}
+
+int main()
+{
+    char Arr[10];
+    int iRet = 0;
+    printf("Enter your name : ");
+    scanf("%[^'\n']s", Arr);
+
+    iRet = CountA(Arr);
+    printf("Count Of a is : %d\n", iRet);
+
+    return 0;
+}
