a5a1fe482b6cb538c0b221380627cea4d5c5cd4f
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-04-21 15:21:09 +0530

    Accept Char from user & check itis small caseornot

---
+++ Assignment21_4.c
@@ -0,0 +1,50 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Function Name :  ChkSmall()  
+// Description   :  Accept Character from user and check whether it is small case or not
+// Input         :  Integer, String
+// Output        :  Integer, String
+// Author        :  Prasad Dangare
+// Date          :  23 Mar 2021
+//
+////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL ChkSmall(char ch)
+{
+    if((ch >= 'a') && (ch <= 'z'))
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter the character : ");
+    scanf("%c", &cValue);
+    
+    bRet = ChkSmall(cValue);
+    if(bRet == TRUE)
+    {
+        printf("It is Small case Character");
+    }
+    else
+    {
+        printf("It is not a Small case Character");
+    }
+
+    return 0;
+}
