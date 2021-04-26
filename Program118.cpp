bb7944438d6d18ac35717ad6b41ab5cca7a91e57
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-04-26 15:25:44 +0530

    First Application In C++

---
+++ Program118.cpp
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////
+//
+// Function Name :  ----()
+// Description   :  First Application In C++  
+// Input         :  ----
+// Output        :  ----
+// Author        :  Prasad Dangare
+// Date          :  26 Mar 2021
+//
+///////////////////////////////////////////////////////////
+
+#include<iostream> // input, output stream 
+
+using namespace std; // std navachi namespace ahei its like header files, which contains related classes
+
+int main()
+{
+    cout << "First Application In C++\n";  // printf("Jay Ganesh from C++\n");
+    
+    return 0;
+}
