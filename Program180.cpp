5ca4df7f22037aaa0be0056f045a9989ebe7ee58
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-05-30 17:39:13 +0530

    Generic Program To Accept No & Find Maximum Number

---
+++ Program180.cpp
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////////
+//
+// Description   :  Generic Program To Accept Number From User And Find Maximum Number 
+// Input         :  Int
+// Output        :  Int
+// Author        :  Prasad Dangare
+// Date          :  29 May 2021
+//
+///////////////////////////////////////////////////////////
+
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+
+T Max(T iNo1, T iNo2)
+{
+    if(iNo1 > iNo2)
+    {
+        return iNo1;
+    }
+    else
+    {
+        return iNo2;
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0, iRet = 0;
+
+    cout << "Enter First Value : \n";
+    cin >> iValue1;
+
+    cout << "Enter Second Value : \n";
+    cin >> iValue2;
+
+    iRet = Max(iValue1, iValue2); // Swap(iValue1,iValue2)
+    
+    cout << "Maximum Is : " << iRet << "\n";
+
+    return 0;
+}
