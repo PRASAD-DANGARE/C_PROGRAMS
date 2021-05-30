98838b63c65a19d543c1b3d8b3585523e0471455
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-05-30 17:39:43 +0530

    Accept N Number From User And Find Maximum Number

---
+++ Program182.cpp
@@ -0,0 +1,52 @@
+///////////////////////////////////////////////////////////
+//
+// Description   :  Accept N Number From User And Find Maximum Number
+// Input         :  Int
+// Output        :  Int
+// Author        :  Prasad Dangare
+// Date          :  29 May 2021
+//
+///////////////////////////////////////////////////////////
+
+#include <iostream>
+
+using namespace std;
+
+int Maximum(int Arr[], int iSize)
+{
+    int iMax = Arr[0];
+
+    for(int i = 1; i < iSize; i++)
+    {
+        if(Arr[i] > iMax)
+        {
+            iMax = Arr[i];
+        }
+    }
+    return iMax;
+}
+
+int main()
+{
+    int *arr = NULL;
+    int iSize = 0, iRet = 0;
+
+    cout << "Enter Number of Elements : ";
+    cin >> iSize;
+
+    arr = new int[iSize];
+
+    cout << "Enter The Values : \n";
+
+    for(int i = 0; i < iSize; i++)
+    {
+        cin >> arr[i];
+    }
+
+    iRet = Maximum(arr, iSize);
+    cout << "Largest Number is : " << iRet << "\n";
+
+    delete [] arr;
+
+    return 0;
+}
